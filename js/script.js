728abb492c9644df800ac8de36b24bf8236e29c1
Alex100688 <alessiozingaro2@gmail.com>
2022-10-26 17:43:16 +0200

    Aggiunge le funzioni di un addEventListener

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 "use strict"
 
-const image=["img/01.jpg, img/02.jpg, img/03.jpg, img/04.jpg, img/05.jpg"];
-console.log(image);
+const image=[`img/01.jpg, img/02.jpg, img/03jpg, img/04.jpg, img/05.jpg`];
+//console.log(image)
+
+let figures=document.getElementsByClassName=("top");
+top.addEventListener(`click`, function(){
+    
+})
+for (let i = 0; i < image.length; i++) {
+
+}
+//console.log(image)
